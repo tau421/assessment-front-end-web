ec2f71d9ff00e7245d28ab592056b9f579cd7f77
Kyle Wilson <tau421@gmail.com>
2022-09-24 23:08:53 -0500

    part 2 complete

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Submission successful!');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function userCompliment(evt) {
+	alert('Looking good!')
+}
+
+let picCompliment = document.querySelector('img')
+
+picCompliment.addEventListener('mouseover', userCompliment)
